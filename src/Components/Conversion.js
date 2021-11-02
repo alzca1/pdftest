617701bd719183d807e9a17bb99d57e471109402
Álvaro Zájara <alzca1@gmail.com>
2021-11-02 13:30:29 +0100

    got proper measures for 4label-page

--- src/Components/Conversion.js
+++ src/Components/Conversion.js
@@ -1,11 +1,17 @@
 import { Button } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
-import { decodeFromBase64, LineJoinStyle, PDFDocument } from "pdf-lib";
+import { decodeFromBase64, degrees, LineJoinStyle, PDFDocument } from "pdf-lib";
 import { pdfObject } from "../pdfObject";
+import { decode } from "base64-arraybuffer";
+import FileSaver from "file-saver";
 
 export default function Conversion() {
+
+    useEffect(() => {
+        console.log("saving...")
+    })
   const [downloadLink, setDownloadLink] = useState(null);
   const fonts = {
     Roboto: {
@@ -19,29 +25,88 @@ export default function Conversion() {
     },
   };
 
- 
+//   const handleFullPage = async () => {
+//     const labelsArray = [];
 
-  const handlePDF = async () => {
-    const pdfSticker1 = decodeFromBase64(pdfObject[0].label); 
+//     pdfObject.forEach(async (pdf) => {
+//       const label = await PDFDocument.load(pdf);
+//       labelsArray.push(label);
+//     });
+
+//     const pdfDoc = await PDFDocument.create();
+//     const [sticker1] = await pdfDoc.embedPdf(labelsArray[0]);
+//     const [sticker2] = await pdfDoc.embedPdf(labelsArray[1]);
+//     const [sticker3] = await pdfDoc.embedPdf(labelsArray[2]);
+//     const [sticker4] = await pdfDoc.embedPdf(labelsArray[3]);
+
+//     const page = pdfDoc.addPage(); 
+    
+//     page.drawPage(sticker1, {})
+//   };
+
+  const handleHalfPage = async () => {
+    const base64label1 = pdfObject[0].label;
+    const base64label2 = pdfObject[1].label;
+    let label1 = await PDFDocument.load(base64label1);
+    let label2 = await PDFDocument.load(base64label2);
+
     const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage();
-    page.drawText("this is my pdf", { x: 25, y: 800 });
+    const [sticker1] = await pdfDoc.embedPdf(label1);
+    const [sticker2] = await pdfDoc.embedPdf(label2);
+
+    
+
+    const page = pdfDoc.addPage([595.28, 841.89]);
+
+    console.log("width", page.getWidth(), "height", page.getHeight())
+
+    // sticker1 x -311.82 - y - 841.89
+    // page.drawPage(sticker1, {
+    //     x: page.getWidth() - 283.46, // 311.82
+    //     y: page.getHeight(), // 841.89
+    //     xScale: 1,
+    //     yScale: 1,
+    //     rotate: degrees(-90)
+    //   });
+   
+      // sticker2 x-311.82 - y 416.7
+    // page.drawPage(sticker2, {
+    //   x: page.getWidth() - 283.46, 
+    //   y: page.getHeight() 
+    //   xScale: 1,
+    //   yScale: 1,
+    //   rotate: degrees(-90)
+    // });
+
+    // sticker3
+     page.drawPage(sticker1, {
+      x: 0,
+      y: page.getHeight(),
+      xScale: 1,
+      yScale: 1,
+      rotate: degrees(-90)
+    });
+
+    // sticker4
+    page.drawPage(sticker2, {
+      x: 0,
+      y: page.getHeight() - 425.19,
+      xScale: 1,
+      yScale: 1,
+      rotate: degrees(-90)
+    });
+    
+
     const pdfBytes = await pdfDoc.save();
 
-    const pdfDocument = new Blob([pdfDoc], { type: "text/pdf" });
-    const link = document.createElement("a");
-    link.href = window.URL.createObjectURL(pdfDocument);
-    link.download ="myPdf.pdf"; 
-    link.click();
-    
+    let blob = new Blob([pdfBytes], { type: "text/pdf" });
+    FileSaver.saveAs(blob, "mypdf.pdf");
   };
-
-
 
   return (
     <>
       <h1>PDF Conversion</h1>
-      <Button onClick={handlePDF} variant="outlined">
+      <Button onClick={handleHalfPage} variant="outlined">
         Get PDF
       </Button>
     </>
--- src/Components/prueba.js
+++ src/Components/prueba.js
@@ -1,48 +1,98 @@
-import { PDFDocument } from 'pdf-lib'
 
-async function embedPdfPages() {
+    const { PDFDocument } = PDFLib
 
-    // 1) Definimos las url de los pdf
-  const flagUrl = 'https://pdf-lib.js.org/assets/american_flag.pdf';
-  const constitutionUrl = 'https://pdf-lib.js.org/assets/us_constitution.pdf';
-// 2) Hacemos el fetch de cada pdf y les pasamos el 
-// arrayBuffer (no necesario porque recibimos en base64)
-  const flagPdfBytes = await fetch(flagUrl).then((res) => res.arrayBuffer());
-  const constitutionPdfBytes = await fetch(constitutionUrl).then((res) =>
-    res.arrayBuffer(),
-  );
-// 3) Creamos el documento pdfDoc
-  const pdfDoc = await PDFDocument.create();
-// 4 Incrustamos la bandera en el pdfDoc y la guardamos en la constante
-// americanFlag
-  const [americanFlag] = await pdfDoc.embedPdf(flagPdfBytes);
-// 5) Guardamos en una constante la carga de constitutionPdfBytes (para qué???)
-  const usConstitutionPdf = await PDFDocument.load(constitutionPdfBytes);
-  // Guardamos en la constante preamble el incrustamiento de la segunda página
-  // de la constante usConstitutionPdf y le añadimos un objeto con las 
-  // coordenadas. 
-  const preamble = await pdfDoc.embedPage(usConstitutionPdf.getPages()[1], {
-    left: 55,
-    bottom: 485,
-    right: 300,
-    top: 575,
-  });
+    async function embedPdfPages() {
+    	// Fetch American flag PDF
+      const flagUrl = 'https://pdf-lib.js.org/assets/american_flag.pdf';
+      const flagPdfBytes = await fetch(flagUrl).then((res) => res.arrayBuffer());
 
-  const americanFlagDims = americanFlag.scale(0.3);
-  const preambleDims = preamble.scale(2.25);
+			// Fetch U.S. constitution PDF
+      const constitutionUrl = 'https://pdf-lib.js.org/assets/us_constitution.pdf';
+      const constitutionPdfBytes = await fetch(constitutionUrl).then((res) =>
+        res.arrayBuffer(),
+      );
 
-  const page = pdfDoc.addPage();
+			// Create a new PDFDocument
+      const pdfDoc = await PDFDocument.create();
 
-  page.drawPage(americanFlag, {
-    ...americanFlagDims,
-    x: page.getWidth() / 2 - americanFlagDims.width / 2,
-    y: page.getHeight() - americanFlagDims.height - 150,
-  });
-  page.drawPage(preamble, {
-    ...preambleDims,
-    x: page.getWidth() / 2 - preambleDims.width / 2,
-    y: page.getHeight() / 2 - preambleDims.height / 2 - 50,
-  });
+			// Embed the first page of the American flag PDF
+      const [americanFlag] = await pdfDoc.embedPdf(flagPdfBytes);
 
-  const pdfBytes = await pdfDoc.save();
-}
+			// Load the constitution PDF into a PDFDocument
+      const usConstitutionPdf = await PDFDocument.load(constitutionPdfBytes);
+
+			// Embed the second page of the constitution and clip the preamble
+      const preamble = await pdfDoc.embedPage(usConstitutionPdf.getPages()[1], {
+        left: 55,
+        bottom: 485,
+        right: 300,
+        top: 575,
+      });
+
+			// Get the width/height of the American flag PDF scaled down to 30% of its original size
+      const americanFlagDims = americanFlag.scale(0.3);
+
+			// Get the width/height of the preamble clipping scaled up to 225% of its original size
+      const preambleDims = preamble.scale(2.25);
+
+			// Add a blank page to the document
+      const page = pdfDoc.addPage();
+
+			// Draw the American flag image in the center top of the page
+      page.drawPage(americanFlag, {
+        ...americanFlagDims,
+        x: page.getWidth() / 2 - americanFlagDims.width / 2,
+        y: page.getHeight() - americanFlagDims.height - 150,
+      });
+
+			// Draw the preamble clipping in the center bottom of the page
+      page.drawPage(preamble, {
+        ...preambleDims,
+        x: page.getWidth() / 2 - preambleDims.width / 2,
+        y: page.getHeight() / 2 - preambleDims.height / 2 - 50,
+      });
+
+      // Serialize the PDFDocument to bytes (a Uint8Array)
+      const pdfBytes = await pdfDoc.save()
+
+			// Trigger the browser to download the PDF document
+      download(pdfBytes, "pdf-lib_pdf_page_embedding_example.pdf", "application/pdf");
+    }
+ 
+
+    const handleProperPDF = async () => {
+        // Constanteamos la primera pegatina decodeada
+        // const pdfSticker = decodeFromBase64(pdfObject[0].label);
+        const pdfSticker1 = decode(pdfObject[0].label);
+        console.log(pdfSticker1);
+        // const pdfSticker1 = new Blob([pdfSticker], {type: "text/pdf"})
+        // Creamos el documento
+        const pdfDoc = await PDFDocument.create();
+        // incrustamos el sticker
+        const [sticker1] = await pdfDoc.embedPdf(pdfSticker1, [0]);
+        console.log(sticker1);
+        const page = pdfDoc.addPage();
+        page.drawPage(sticker1, {
+          x: page.getWidth() / 2 - sticker1.width / 2,
+          y: page.getHeight() - sticker1.height - 150,
+          xScale: 0.5,
+          yScale: 0.5,
+        });
+        const pdfBytes = await pdfDoc.save();
+        console.log(pdfBytes);
+    
+        // const pdfDocument = new Blob([pdfBytes], { type: "text/pdf" });
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(pdfBytes);
+        link.download = "myPdf.pdf";
+        link.click();
+      };
+
+      const handle = async () => {
+        const base64 = pdfObject[0].label;
+        let pdfDoc = await PDFDocument.load(base64);
+        const pdfBytes = await pdfDoc.save();
+       
+        let blob = new Blob([pdfBytes], { type: "text/pdf" });
+        FileSaver.saveAs(blob, "mypdf.pdf");
+      };
